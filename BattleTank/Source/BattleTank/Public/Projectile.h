6c9bd791e679bd04f48e659a02e24ed820070cc5
Trey Trimble <bagaintrey@gmail.com>
2017-08-30 21:49:18 +0100

    Added Components For particle Into Projectile Folder From Starter Content

--- BattleTank/Source/BattleTank/Public/Projectile.h
+++ BattleTank/Source/BattleTank/Public/Projectile.h
@@ -7,6 +7,9 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Projectile.generated.h"
 
+class UStaticMeshComponent;
+class UParticleSystemComponent;
+
 UCLASS()
 class BATTLETANK_API AProjectile : public AActor
 {
@@ -28,6 +31,11 @@ public:
     
 private:
     UProjectileMovementComponent* ProjectileMovement = nullptr;
-	
+    
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UStaticMeshComponent* CollisionMesh = nullptr;
+    
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    UParticleSystemComponent* LaunchBlast = nullptr;
 	
 };
